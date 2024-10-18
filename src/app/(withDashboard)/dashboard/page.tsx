64cdd1a01865d4ed7287eca78378412bf6146b51
sabrina <sabrinaahmednitu@gmail.com>
2024-10-19 03:06:47 +0600

    google and github authentication done

--- src/app/(withDashboard)/dashboard/page.tsx
+++ src/app/(withDashboard)/dashboard/page.tsx
@@ -1,7 +1,27 @@
-const DashboardPage = () => {
+import { authOptions } from '@/utils/authOptions';
+import { getServerSession } from 'next-auth';
+import Image from 'next/image';
+
+const DashboardPage = async () => {
+  const session = await getServerSession(authOptions);
+  console.log(session);
   return (
     <div>
-      <h1 className="text-4xl text-center mt-10">Welcome To Dashboard Page</h1>
+      {session?.user && (
+        <>
+          <Image
+            src={session?.user?.image}
+            width={200}
+            height={200}
+            className="rounded-full mx-auto mt-11"
+            alt="user image"
+          ></Image>
+          <h1 className="text-4xl text-center mt-5">
+            Welcome {session?.user?.name}
+          </h1>
+          <h4 className="text-2xl text-center mt-3">{session?.user?.email}</h4>
+        </>
+      )}
     </div>
   );
 };
